ad8b49565bc5bb8b2396c405eb91487aca492e1b
ozkan <ozkancil92@gmail.com>
2020-03-21 22:22:32 -0400

    commit 3

--- src/gitPackage02/GitDay02.java
+++ src/gitPackage02/GitDay02.java
@@ -5,5 +5,6 @@ public static void main(String[] args) {
 	System.out.println("hello git");
 	System.out.println("hello git users");
 	System.out.println("yellow");
+	System.out.println("commit 7");
 }
 }
