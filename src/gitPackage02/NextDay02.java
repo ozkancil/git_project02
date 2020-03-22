b3044e4e8c1970e59f787e230f9cd5499d608d95
ozkan <ozkancil92@gmail.com>
2020-03-21 22:48:24 -0400

    commit 10

--- src/gitPackage02/NextDay02.java
+++ src/gitPackage02/NextDay02.java
@@ -2,6 +2,11 @@ package gitPackage02;
 
 public class NextDay02 {
 public static void main(String[] args) {
-	System.out.println("Hello dear friends");
+	System.out.println("Hello dear Testers");
+	
+	
+}
+public static void gitHub() {
+	
 }
 }
